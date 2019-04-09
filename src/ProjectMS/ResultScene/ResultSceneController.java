d256eb303fce78e52bdcc7ece16e6b18fb184f35
Yamashtia Keisuke <kei95@Yamashtias-Air.hitronhub.home>
2019-04-09 09:31:50 -0700

    Added Timer and Changed some GUI

---
+++ src/ProjectMS/ResultScene/ResultSceneController.java
@@ -0,0 +1,4 @@
+package ProjectMS.ResultScene;
+
+public class ResultSceneController {
+}
--- src/ProjectMS/StarterScene/StarterSceneController.java
+++ src/ProjectMS/StarterScene/StarterSceneController.java
@@ -1,15 +1,17 @@
-package ProjectMS.Title;
+package ProjectMS.StarterScene;
 import ProjectMS.Main;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
 
-public class Controller {
+
+public class StarterScreenController {
 
     @FXML
     private Button startButton;
+
     @FXML
     public void startButtonPressed(ActionEvent actionEvent) {
-        Main.screenController.activate("MainScene");
+        Main.screenController.activate("TimerScene");
     }
 }
