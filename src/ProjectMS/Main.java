88265e1595be08a4bb5f2251dc1eed5a47f3646d
Rtihai <rtihai@gmail.com>
2019-04-10 19:40:37 -0700

    Java FX - job done, last version

--- src/ProjectMS/Main.java
+++ src/ProjectMS/Main.java
@@ -17,7 +17,15 @@ public class Main extends Application {
         Scene scene = new Scene(root);
         screenController = new ScreenController(scene);
         screenController.addScreen("StarterScene", FXMLLoader.load(getClass().getResource("StarterScene/StarterScene.fxml")));
+
         screenController.addScreen("TimerScene", FXMLLoader.load(getClass().getResource("TimerScene/TimerScene.fxml")));
+        screenController.addScreen("TimerScene1", FXMLLoader.load(getClass().getResource("TimerScene/TimerScene1.fxml")));
+        screenController.addScreen("TimerScene2", FXMLLoader.load(getClass().getResource("TimerScene/TimerScene2.fxml")));
+        screenController.addScreen("TimerScene3", FXMLLoader.load(getClass().getResource("TimerScene/TimerScene3.fxml")));
+        screenController.addScreen("TimerScene4", FXMLLoader.load(getClass().getResource("TimerScene/TimerScene4.fxml")));
+        screenController.addScreen("TimerScene5", FXMLLoader.load(getClass().getResource("TimerScene/TimerScene5.fxml")));
+        screenController.addScreen("TimerScene6", FXMLLoader.load(getClass().getResource("TimerScene/TimerScene6.fxml")));
+
         screenController.addScreen("ResultScene", FXMLLoader.load(getClass().getResource("ResultScene/ResultScene.fxml")));
 
         scene.getStylesheets().addAll(this.getClass().getResource("./style.css").toExternalForm());
--- src/ProjectMS/TimerScene/TimerSceneController.java
+++ src/ProjectMS/TimerScene/TimerSceneController.java
@@ -21,6 +21,8 @@ public class TimerSceneController implements Initializable {
     @FXML
     public Button startTimerButton;
 
+    @FXML Button move;
+
     @FXML
     private Label dateTime;
 
@@ -48,5 +50,35 @@ public class TimerSceneController implements Initializable {
     public void startButtonPressed(ActionEvent actionEvent) {
         Main.screenController.activate("ResultScene");
     }
+    @FXML
+    public void startButtonPressed1(ActionEvent actionEvent) {
+        Main.screenController.activate("TimerScene1");
+    }
+    @FXML
+    public void startButtonPressed2(ActionEvent actionEvent) {
+        Main.screenController.activate("TimerScene2");
+    }
+    @FXML
+    public void startButtonPressed3(ActionEvent actionEvent) {
+        Main.screenController.activate("TimerScene3");
+    }
+    @FXML
+    public void startButtonPressed4(ActionEvent actionEvent) {
+        Main.screenController.activate("TimerScene4");
+    }
+    @FXML
+    public void startButtonPressed5(ActionEvent actionEvent) {
+        Main.screenController.activate("TimerScene5");
+    }
+    @FXML
+    public void startButtonPressed6(ActionEvent actionEvent) {
+        Main.screenController.activate("TimerScene6");
+    }
+    @FXML
+    public void startButtonPressed7(ActionEvent actionEvent) {
+        Main.screenController.activate("TimerScene");
+    }
+
+
 
 }
